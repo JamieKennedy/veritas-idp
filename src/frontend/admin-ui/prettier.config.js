a10aebb56ab80484e2f4e03086d029227e91d9ba
jamie <jamiekennedy97@gmail.com>
2026-06-12 14:44:14 +0100

    frontend init

---
+++ src/frontend/admin-ui/prettier.config.js
@@ -0,0 +1,12 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 4,
+}
+
+export default config
